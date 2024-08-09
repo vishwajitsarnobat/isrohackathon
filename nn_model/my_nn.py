d7b7b9e5b87a41ec13ca9535d5d195efb01cc165
Vishwajit <sambhaji2772@gmail.com>
2024-08-09 20:06:08 +0530

    first working nn model

--- nn_model/my_nn.py
+++ nn_model/my_nn.py
@@ -1,261 +1,206 @@
-'''
-errors to work on:
-make ot atleat work
-use time difference as a parameter for accurate prediction
-'''
-
-
 import xarray as xr
 import numpy as np
 import os
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score
+from datetime import datetime
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv3D, ConvLSTM2D, BatchNormalization, Dense, Flatten, Concatenate, Dropout, Reshape
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.utils import Sequence
+from tensorflow.keras.layers import Input, Conv3D, LSTM, Dense, Flatten, Concatenate, TimeDistributed
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import matplotlib.pyplot as plt
 
-# Preprocessing functions
-def extract_variables(file_path, variable):
-    ds = xr.open_dataset(file_path)
-    var = ds[variable].values
-
-    # Replace NaN values with zero
-    var = np.nan_to_num(var, nan=0.0)
-
-    # Extracting a 5x5x5 grid from the middle
-    mid_height = var.shape[1] // 2
-    mid_lat = var.shape[2] // 2
-    mid_lon = var.shape[3] // 2
-
-    var = var[:, 
-              mid_height - 2:mid_height + 3, 
-              mid_lat - 2:mid_lat + 3, 
-              mid_lon - 2:mid_lon + 3]
-
-    ds.close()
-    return var
-
-def standardize(data):
-    # Replace NaN values with zero before standardizing
-    data = np.nan_to_num(data, nan=0.0)
-    scaler = StandardScaler()
-    reshaped_data = data.reshape(-1).astype(np.float32)  # Flatten the data
-    standardized_data = scaler.fit_transform(reshaped_data[:, np.newaxis]).reshape(data.shape)
-    return standardized_data
-
-def load_and_preprocess_data(folder_path):
-    velocity_list = []
-    reflectivity_list = []
+def load_and_preprocess_data(folder_path, grid_size=5):
+    data = []
     timestamps = []
-    file_info = []
-    
-    for file_name in os.listdir(folder_path):
+
+    for file_name in sorted(os.listdir(folder_path)):
         if file_name.endswith('.nc'):
             file_path = os.path.join(folder_path, file_name)
-            ds = xr.open_dataset(file_path)
-            timestamp = ds['time'].values[0]
-            timestamps.append(timestamp)
-            file_info.append((file_path, timestamp))
-            ds.close()
+            with xr.open_dataset(file_path) as ds:
+                timestamp = ds['time'].values[0]
+                timestamps.append(timestamp)
 
-    file_info.sort(key=lambda x: x[1])
-    
-    for file_path, _ in file_info: # to arrange data in timely order
-        radial_velocity = extract_variables(file_path, 'VEL')
-        reflectivity = extract_variables(file_path, 'DBZ')
+                reflectivity = ds['DBZ'].values[0]
+                velocity = ds['VEL'].values[0]
 
-        standardized_velocity = standardize(radial_velocity)
-        standardized_reflectivity = standardize(reflectivity)
-        
-        velocity_list.append(standardized_velocity)
-        reflectivity_list.append(standardized_reflectivity)
+                # Replace NaN values with the mean of the non-NaN values
+                reflectivity = np.nan_to_num(reflectivity, nan=np.nanmean(reflectivity))
+                velocity = np.nan_to_num(velocity, nan=np.nanmean(velocity))
 
-    return np.array(velocity_list), np.array(reflectivity_list), np.array(timestamps)
+                center_h, center_lat, center_lon = [s // 2 for s in reflectivity.shape]
+                half_grid = grid_size // 2
 
-def create_sequences(velocity_data, reflectivity_data, seq_length=3):
-    X_velocity, X_reflectivity, y_velocity, y_reflectivity = [], [], [], []
-    
-    for i in range(len(velocity_data) - seq_length):
-        seq_X_velocity = velocity_data[i]
-        seq_X_reflectivity = reflectivity_data[i]
-        seq_y_velocity = velocity_data[i + seq_length]
-        seq_y_reflectivity = reflectivity_data[i + seq_length]
-        
-        X_velocity.append(seq_X_velocity)
-        X_reflectivity.append(seq_X_reflectivity)
-        y_velocity.append(seq_y_velocity)
-        y_reflectivity.append(seq_y_reflectivity)
-    
-    X_velocity = np.array(X_velocity)
-    X_reflectivity = np.array(X_reflectivity)
-    y_velocity = np.array(y_velocity)
-    y_reflectivity = np.array(y_reflectivity)
-    
-    # Reshape to match the model's expected input
-    X_velocity = X_velocity[..., np.newaxis]  # Adding the channel dimension
-    X_reflectivity = X_reflectivity[..., np.newaxis]  # Adding the channel dimension
-    y_velocity = y_velocity[..., np.newaxis]  # Adding the channel dimension
-    y_reflectivity = y_reflectivity[..., np.newaxis]  # Adding the channel dimension
-    
-    return X_velocity, X_reflectivity, y_velocity, y_reflectivity
+                slices = [slice(center - half_grid, center + half_grid + 1) 
+                          for center in (center_h, center_lat, center_lon)]
 
-def create_tf_dataset(X_velocity, X_reflectivity, y_velocity, y_reflectivity, batch_size=32, shuffle=True):
-    def generator():
-        for i in range(len(X_velocity)):
-            # Remove the extra dimension from the input
-            yield (
-                np.squeeze(X_velocity[i]), 
-                np.squeeze(X_velocity[i]), 
-                np.squeeze(X_reflectivity[i]), 
-                np.squeeze(X_reflectivity[i])
-            ), (
-                np.squeeze(y_velocity[i]), 
-                np.squeeze(y_reflectivity[i])
-            )
+                ref_grid = reflectivity[tuple(slices)]
+                vel_grid = velocity[tuple(slices)]
 
-    output_signature = (
-        (
-            tf.TensorSpec(shape=spatial_input_shape, dtype=tf.float32),
-            tf.TensorSpec(shape=temporal_input_shape, dtype=tf.float32),
-            tf.TensorSpec(shape=spatial_input_shape, dtype=tf.float32),
-            tf.TensorSpec(shape=temporal_input_shape, dtype=tf.float32),
-        ),
-        (
-            tf.TensorSpec(shape=spatial_input_shape[:-1], dtype=tf.float32),
-            tf.TensorSpec(shape=spatial_input_shape[:-1], dtype=tf.float32),
-        )
-    )
-    
-    dataset = tf.data.Dataset.from_generator(
-        generator,
-        output_signature=output_signature
-    )
+                combined_data = np.stack([ref_grid, vel_grid], axis=-1)
+                data.append(combined_data)
 
-    if shuffle:
-        dataset = dataset.shuffle(buffer_size=len(X_velocity))
-    
-    dataset = dataset.batch(batch_size)
-    return dataset
+    return np.array(data), np.array(timestamps)
 
-# Model building functions
-def create_spatial_model(input_shape):
-    inputs = Input(shape=input_shape)  # (5, 5, 5, 1)
-    x = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(inputs)
-    x = BatchNormalization()(x)
-    x = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Flatten()(x)
-    return Model(inputs, x)
+def create_sequences(data, timestamps, input_seq_length=3, forecast_horizon=3):
+    X, y, time_diffs = [], [], []
+    
+    for i in range(len(data) - input_seq_length - forecast_horizon + 1):
+        X.append(data[i:i+input_seq_length])
+        y.append(data[i+input_seq_length+forecast_horizon-1])
+        time_diff = (timestamps[i+input_seq_length+forecast_horizon-1] - timestamps[i]).astype('timedelta64[m]').astype(int)
+        time_diffs.append(time_diff)
 
-def create_temporal_model(input_shape):
-    inputs = Input(shape=input_shape)  # (seq_length, 5, 5, 1)
-    x = ConvLSTM2D(32, (3, 3), activation='relu', padding='same', return_sequences=True)(inputs)
-    x = BatchNormalization()(x)
-    x = ConvLSTM2D(64, (3, 3), activation='relu', padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Flatten()(x)
-    return Model(inputs, x)
+    return np.array(X), np.array(y), np.array(time_diffs)
 
-def create_radarcast_net(spatial_input_shape, temporal_input_shape):
-    # Velocity models
-    spatial_model_velocity = create_spatial_model(spatial_input_shape)
-    temporal_model_velocity = create_temporal_model(temporal_input_shape)
+def build_model(input_shape, output_shape):
+    inputs = Input(shape=input_shape)
     
-    # Reflectivity models
-    spatial_model_reflectivity = create_spatial_model(spatial_input_shape)
-    temporal_model_reflectivity = create_temporal_model(temporal_input_shape)
+    x = TimeDistributed(Conv3D(32, (3, 3, 3), activation='relu', padding='same'))(inputs)
+    x = TimeDistributed(Conv3D(64, (3, 3, 3), activation='relu', padding='same'))(x)
     
-    # Combine inputs
-    combined_input_velocity = Concatenate()([spatial_model_velocity.output, temporal_model_velocity.output])
-    combined_input_reflectivity = Concatenate()([spatial_model_reflectivity.output, temporal_model_reflectivity.output])
+    x = TimeDistributed(Flatten())(x)
     
-    combined = Concatenate()([combined_input_velocity, combined_input_reflectivity])
+    x = LSTM(128, return_sequences=False)(x)
     
-    # Fully connected layers
-    x = Dense(128, activation='relu')(combined)
-    x = Dropout(0.3)(x)
+    time_input = Input(shape=(1,))
+    
+    x = Concatenate()([x, time_input])
+    
     x = Dense(64, activation='relu')(x)
-    x = Dropout(0.3)(x)
+    x = Dense(32, activation='relu')(x)
     
-    # Output layers
-    output_velocity = Dense(np.prod(spatial_input_shape[:-1]), activation='linear', name='velocity_output')(x)
-    output_velocity = Reshape(spatial_input_shape[:-1])(output_velocity)
+    output = Dense(np.prod(output_shape), activation='linear')(x)
+    output = tf.keras.layers.Reshape(output_shape)(output)
     
-    output_reflectivity = Dense(np.prod(spatial_input_shape[:-1]), activation='linear', name='reflectivity_output')(x)
-    output_reflectivity = Reshape(spatial_input_shape[:-1])(output_reflectivity)
-    
-    model = Model(inputs=[spatial_model_velocity.input, temporal_model_velocity.input, 
-                          spatial_model_reflectivity.input, temporal_model_reflectivity.input], 
-                  outputs=[output_velocity, output_reflectivity])
-    
+    model = Model(inputs=[inputs, time_input], outputs=output)
     return model
 
-folder_path = '/home/vishwajitsarnobat/Downloads/isro_hackathon_data'
-velocity_data, reflectivity_data, timestamps = load_and_preprocess_data(folder_path)
+def analyze_predictions(predictions, y_test, threshold=35):
+    precipitation_detected = []
+    movement_vectors = []
 
-seq_length = 3 
-X_velocity, X_reflectivity, y_velocity, y_reflectivity = create_sequences(velocity_data, reflectivity_data, seq_length=seq_length)
+    for pred, true in zip(predictions, y_test):
+        pred_reflectivity = pred[..., 0]
+        true_reflectivity = true[..., 0]
 
-train_size = int(0.7 * len(X_velocity))
-val_size = int(0.15 * len(X_velocity))
+        pred_precipitation = pred_reflectivity > threshold
+        true_precipitation = true_reflectivity > threshold
 
-# dividing into sets
-train_X_velocity = X_velocity[:train_size]
-val_X_velocity = X_velocity[train_size:train_size + val_size]
-test_X_velocity = X_velocity[train_size + val_size:]
+        precipitation_detected.append({
+            'predicted': pred_precipitation.any(),
+            'actual': true_precipitation.any()
+        })
 
-train_X_reflectivity = X_reflectivity[:train_size]
-val_X_reflectivity = X_reflectivity[train_size:train_size + val_size]
-test_X_reflectivity = X_reflectivity[train_size + val_size:]
+        if pred_precipitation.any() and true_precipitation.any():
+            pred_com = np.array(np.where(pred_precipitation)).mean(axis=1)
+            true_com = np.array(np.where(true_precipitation)).mean(axis=1)
+            movement = true_com - pred_com
+            movement_vectors.append(movement)
+        else:
+            movement_vectors.append(None)
 
-train_y_velocity = y_velocity[:train_size]
-val_y_velocity = y_velocity[train_size:train_size + val_size]
-test_y_velocity = y_velocity[train_size + val_size:]
+    return precipitation_detected, movement_vectors
 
-train_y_reflectivity = y_reflectivity[:train_size]
-val_y_reflectivity = y_reflectivity[train_size:train_size + val_size]
-test_y_reflectivity = y_reflectivity[train_size + val_size:]
+def plot_reflectivity(actual, predicted, slice_index=2):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    
+    im1 = ax1.imshow(actual[slice_index, :, :], cmap='viridis')
+    ax1.set_title('Actual Reflectivity')
+    plt.colorbar(im1, ax=ax1)
+    
+    im2 = ax2.imshow(predicted[slice_index, :, :], cmap='viridis')
+    ax2.set_title('Predicted Reflectivity')
+    plt.colorbar(im2, ax=ax2)
+    
+    plt.tight_layout()
+    plt.show()
 
-spatial_input_shape = (5, 5, 5, 1)
-temporal_input_shape = (seq_length, 5, 5, 1)
+def plot_temporal_evolution(actual_sequence, predicted_future, time_diff):
+    fig, axes = plt.subplots(1, 4, figsize=(20, 5))
+    
+    for i in range(3):
+        im = axes[i].imshow(actual_sequence[i, 2, :, :, 0], cmap='viridis')
+        axes[i].set_title(f'T-{2-i}')
+        plt.colorbar(im, ax=axes[i])
+    
+    im = axes[3].imshow(predicted_future[2, :, :, 0], cmap='viridis')
+    axes[3].set_title(f'T+{time_diff} (Predicted)')
+    plt.colorbar(im, ax=axes[3])
+    
+    plt.tight_layout()
+    plt.show()
 
-batch_size = 4  # Adjust as needed
+def main():
+    folder_path = '/home/vishwajitsarnobat/Downloads/isro_hackathon_data'
+    grid_size = 5
+    input_seq_length = 3
+    forecast_horizon = 3
 
-# Create the TensorFlow datasets
-train_dataset = create_tf_dataset(train_X_velocity, train_X_reflectivity, train_y_velocity, train_y_reflectivity, batch_size=batch_size)
-val_dataset = create_tf_dataset(val_X_velocity, val_X_reflectivity, val_y_velocity, val_y_reflectivity, batch_size=batch_size)
-test_dataset = create_tf_dataset(test_X_velocity, test_X_reflectivity, test_y_velocity, test_y_reflectivity, batch_size=batch_size)
+    data, timestamps = load_and_preprocess_data(folder_path, grid_size)
+    X, y, time_diffs = create_sequences(data, timestamps, input_seq_length, forecast_horizon)
 
-# Create the model
-model = create_radarcast_net(spatial_input_shape, temporal_input_shape)
+    X_train, X_test, y_train, y_test, time_diffs_train, time_diffs_test = train_test_split(
+        X, y, time_diffs, test_size=0.2, random_state=42)
 
-# Define callbacks for early stopping and model checkpointing
-early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-model_checkpoint = ModelCheckpoint('radarcast_net.keras', monitor='val_loss', save_best_only=True)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train.reshape(-1, X_train.shape[-1])).reshape(X_train.shape)
+    X_test_scaled = scaler.transform(X_test.reshape(-1, X_test.shape[-1])).reshape(X_test.shape)
+    y_train_scaled = scaler.transform(y_train.reshape(-1, y_train.shape[-1])).reshape(y_train.shape)
+    y_test_scaled = scaler.transform(y_test.reshape(-1, y_test.shape[-1])).reshape(y_test.shape)
 
-# Compile the model with correct output layer names
-model.compile(
-    optimizer='adam', 
-    loss={
-        'velocity_output': 'mse',
-        'reflectivity_output': 'mse'
-    },
-    metrics={
-        'velocity_output': 'mae',
-        'reflectivity_output': 'mae'
-    }
-)
+    input_shape = (input_seq_length, grid_size, grid_size, grid_size, 2)
+    output_shape = (grid_size, grid_size, grid_size, 2)
+    model = build_model(input_shape, output_shape)
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='mse', metrics=['mae'])
 
-# Train the model
-model.fit(
-    train_dataset, 
-    validation_data=val_dataset, 
-    epochs=100, 
-    callbacks=[early_stopping, model_checkpoint]
-)
+    history = model.fit(
+        [X_train_scaled, time_diffs_train], y_train_scaled,
+        validation_split=0.2,
+        epochs=50,
+        batch_size=32,
+        callbacks=[
+            tf.keras.callbacks.EarlyStopping(patience=5, restore_best_weights=True),
+            tf.keras.callbacks.ModelCheckpoint('best_model.keras', save_best_only=True)
+        ]
+    )
 
-# Evaluate the model on the test set
-test_loss, test_metrics = model.evaluate(test_dataset)
-print(f'Test Loss: {test_loss}, Test MAE (Velocity): {test_metrics[1]}, Test MAE (Reflectivity): {test_metrics[2]}')
+    test_loss, test_mae = model.evaluate([X_test_scaled, time_diffs_test], y_test_scaled)
+    print(f"Test Loss: {test_loss}, Test MAE: {test_mae}")
+
+    predictions = model.predict([X_test_scaled, time_diffs_test])
+    predictions = scaler.inverse_transform(predictions.reshape(-1, predictions.shape[-1])).reshape(predictions.shape)
+
+    precipitation_results, movements = analyze_predictions(predictions, y_test)
+
+    correct_detections = sum(1 for res in precipitation_results if res['predicted'] == res['actual'])
+    accuracy = correct_detections / len(precipitation_results)
+    print(f"Precipitation Detection Accuracy: {accuracy:.2f}")
+
+    valid_movements = [m for m in movements if m is not None]
+    if valid_movements:
+        avg_movement = np.mean(valid_movements, axis=0)
+        print(f"Average Movement Vector: {avg_movement}")
+        
+        current_position = np.array([2, 2, 2])
+        future_position = current_position + avg_movement
+        print(f"Predicted future position: {future_position}")
+    else:
+        print("No valid movements detected")
+
+    plot_reflectivity(y_test[0, ..., 0], predictions[0, ..., 0])
+    plot_temporal_evolution(X_test[0], predictions[0], time_diffs_test[0])
+
+    mse = mean_squared_error(y_test.flatten(), predictions.flatten())
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y_test.flatten(), predictions.flatten())
+
+    print(f"Root Mean Squared Error (RMSE): {rmse:.2f}")
+    print(f"Mean Absolute Error (MAE): {mae:.2f}")
+
+    model.save('precipitation_model.keras')
+
+if __name__ == "__main__":
+    main()
+
